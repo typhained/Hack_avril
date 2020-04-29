23f70028091cf7c850cec0ebdf5a507cb4d15ac0
Maxime KAISER <maximek4iser@gmail.com>
2020-04-29 23:19:32 +0200

    add quizz

--- quizz.php
+++ quizz.php
@@ -1,71 +1,71 @@
 <h2>Question n°1</h2>
-<form action="qcm.php" id="quest1" method="post">
+<form action="validate.php" id="quest1" method="post">
     Ce monument est au Kazakhstan ? :
     <input type="submit" name="choice" value="Vrai">
-    <input type="submit" name="choice2" value="Faux"><br>
+    <input type="submit" name="choice" value="Faux"><br>
     <!-- https://www.windy.com/fr/-Webcams/Russie/Oblast-d%27Arkhangelsk/Arkhangelsk/%D0%90%D1%80%D1%85%D0%B0%D0%BD%D0%B3%D0%B5%D0%BB%D1%8C%D1%81%D0%BA-%D0%90%D1%80%D1%85%D0%B0%D0%BD%D0%B3%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F-%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C-%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F/webcams/1521788187?61.617,37.046,5 -->
     <!-- Réponse choice2 -->
 </form>
 
 <h2>Question n°2</h2>
-<form action="qcm.php" id="quest2" method="post">
+<form action="validate.php" id="quest2" method="post">
     Nous sommes en France ? :
-    <input type="submit" name="choice3" value="Vrai">
-    <input type="submit" name="choice4" value="Faux"><br>
+    <input type="submit" name="choice2" value="Vrai">
+    <input type="submit" name="choice2" value="Faux"><br>
     <!-- -->
     <!-- -->
 </form>
 
 <h2>Question n°3</h2>
-<form action="qcm.php" id="quest3" method="post">
+<form action="validate.php" id="quest3" method="post">
     Dans quel pays sommes nous ? :
-    <input type="submit" name="choice5" value="Italie">
-    <input type="submit" name="choice6" value="Espagne"><br>
+    <input type="submit" name="choice3" value="Vrai">
+    <input type="submit" name="choice3" value="Faux"><br>
     <!-- https://www.windy.com/fr/-Webcams/Espagne/Castille-et-Le%C3%B3n/Le%C3%B3n/Catedral-de/webcams/1390070107?38.462,-10.767,5 -->
     <!-- Réponse choice6 -->
 </form>
 
 <h2>Question n°4</h2>
-<form action="qcm.php" id="quest4" method="post">
+<form action="validate.php" id="quest4" method="post">
     ? :
-    <input type="submit" name="choice7" value="Vrai">
-    <input type="submit" name="choice8" value="Faux"><br>
+    <input type="submit" name="choice4" value="Vrai">
+    <input type="submit" name="choice4" value="Faux"><br>
     <!-- -->
     <!-- -->
 </form>
 
 <h2>Question n°5</h2>
-<form action="qcm.php" id="quest5" method="post">
+<form action="validate.php" id="quest5" method="post">
     ? :
-    <input type="submit" name="choice9" value="Vrai">
-    <input type="submit" name="choice10" value="Faux"><br>
+    <input type="submit" name="choice5" value="Vrai">
+    <input type="submit" name="choice5" value="Faux"><br>
     <!-- -->
     <!-- -->
 </form>
 
 <h2>Question n°6</h2>
-<form action="qcm.php" id="quest6" method="post">
+<form action="validate.php" id="quest6" method="post">
      ? :
-    <input type="submit" name="choice11" value="Vrai">
-    <input type="submit" name="choice12" value="Faux"><br>
+    <input type="submit" name="choice6" value="Vrai">
+    <input type="submit" name="choice6" value="Faux"><br>
     <!-- -->
     <!-- -->
 </form>
 
 <h2>Question n°7</h2>
-<form action="qcm.php" id="quest7" method="post">
+<form action="validate.php" id="quest7" method="post">
       ? :
-    <input type="submit" name="choice13" value="Vrai">
-    <input type="submit" name="choice14" value="Faux"><br>
+    <input type="submit" name="choice7" value="Vrai">
+    <input type="submit" name="choice7" value="Faux"><br>
     <!-- -->
     <!-- -->
 </form>
 
 <h2>Question n°8</h2>
-<form action="qcm.php" id="quest8" method="post">
+<form action="validate.php" id="quest8" method="post">
       ? :
-    <input type="submit" name="choice15" value="Vrai">
-    <input type="submit" name="choice16" value="Faux"><br>
+    <input type="submit" name="choice8" value="Vrai">
+    <input type="submit" name="choice8" value="Faux"><br>
     <!-- -->
     <!-- -->
 </form>
