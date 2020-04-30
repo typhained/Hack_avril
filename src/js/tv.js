a88d4eeb6c7c58200b54d32e733bef4e8330d569
karapuce <typhaine.demangeon@gmail.com>
2020-04-30 03:52:38 +0200

    affichage tv

--- src/js/tv.js
+++ src/js/tv.js
@@ -1,3 +1,22 @@
 var day = "2";
 
 localStorage.setItem("day", day);
+
+window.setTimeout(function () {
+    $("#windy").toggleClass("hidden");
+}, 10000);
+
+window.setTimeout(function () {
+    $("iframe").toggleClass("hidden");
+}, 10000);
+
+
+$("#tv-off").click(function(){
+    $('section').animate({
+        opacity: 0
+    }, 1000,function() {
+        //thing to do when you animation is finished e.g.
+        location.href = 'index.php';
+    });
+});
+
