199f0b11ca9b418c297e07fa0ec988435e1f0193
mozbiat <yann_du@hotmail.fr>
2020-04-30 12:35:24 +0200

    modif journal

--- quizz.php
+++ quizz.php
@@ -22,79 +22,74 @@ $temp8 = $cam->getweather(46.21,-63.26);
 ?>
     <div id="question1" class="">
         <img src="<?php echo $photo1['image']?>">
+        <p>(la température actuelle de ce lieu est de <?php echo $temp1;?>°C)</p>
         <div class="quest" >
-            <legend>Question 1 : Ce monument est il au Kazakhstan ou en Russie ? (la température actuelle de ce lieu est de
-            <?php echo $temp1;?>°C)</legend>
+            <legend>Question 1 <br><br>Ce monument est il au Kazakhstan ou en Russie ?</legend>
             <button id="bad">Kazakhstan</button>
             <button id="good">Russie</button>
         </div>
     </div>
     <div id="question2" class="caché">
         <img src="<?php echo $photo2['image']?>">
+        <p>(la température actuelle de ce lieu est de <?php echo $temp2;?>°C)</p>
         <div class="quest" >
-            <legend>Question 2 : Sommes nous en France ? (la température actuelle de ce lieu est de
-                <?php echo $temp2;?>°C)</legend>
+            <legend>Question 2 <br><br> Sommes nous en France ?</legend>
             <button id="bad2">Faux</button>
             <button id="good2">Vrai</button>
-            <!-- 49.372,7.251,7 -->
-            <!-- Réponse FAUX -->
         </div>
     </div>
     <div id="question3" class="caché">
         <img src="<?php echo $photo3['image']?>">
+        <p>(la température actuelle de ce lieu est de <?php echo $temp3;?>°C)</p>
         <div class="quest" >
-                <legend>Question 3 : Sommes nous en Europe ? (la température actuelle de ce lieu est de
-                    <?php echo $temp3;?>°C)</legend>
+                <legend>Question 3 <br><br> Sommes nous en Europe ?</legend>
                 <button id="good3">Faux</button>
                 <button id="bad3">Vrai</button>
-                <!-- 38.462,-10.767,5 -->
-                <!-- Réponse VRAI -->
             </div>
     </div>
     <div id="question4" class="caché">
         <img src="<?php echo $photo4['image']?>">
+        <p> (la température actuelle de ce lieu est de <?php echo $temp4;?>°C)</p>
         <div class="quest" >
-            <legend>Question 4 : En quelle année à été terminé la place St-Pierre ? (la température actuelle de ce lieu est de
-                <?php echo $temp4;?>°C)</legend>
+            <legend>Question 4 <br><br> En quelle année à été terminé la place St-Pierre ?</legend>
             <button id="good4">1667</button>
             <button id="bad4">1982</button>
-            <!-- Réponse VRAI -->
         </div>
     </div>
     <div id="question5" class="caché">
         <img src="<?php echo $photo5['image']?>">
-        <div class="quest" >
-            <legend>Question 5 : Combien mesure l'Oriental Pearl Tv Tower ? (la température actuelle de ce lieu est de
-                <?php echo $temp5;?>°C)</legend>
+        <p>(la température actuelle de ce lieu est de <?php echo $temp5;?>°C)</p>
+        <div class="quest">
+            <legend>Question 5 <br><br> Combien mesure l'Oriental Pearl Tv Tower ?</legend>
             <button id="good5">468m</button>
             <button id="bad5">Russie</button>
         </div>
     </div>
     <div id="question6" class="caché">
         <img src="<?php echo $photo6['image']?>">
+        <p>(la température actuelle de ce lieu est de <?php echo $temp6;?>°C)</p>
         <div class="quest" >
-            <legend>Question 6 : Comment s'orthographie correctement cette ville Islandaise ? (la température actuelle de ce lieu est de
-                <?php echo $temp6;?>°C)</legend>
+            <legend>Question 6 <br><br> Comment s'orthographie correctement cette ville Islandaise ?</legend>
             <button id="bad6">Rekyajvik</button>
             <button id="good6">Reykjavik</button>
         </div>
     </div>
     <div id="question7" class="caché">
+        <p>(la température actuelle de ce lieu est de <?php echo $temp7;?>°C)</p>
         <img src="<?php echo $photo7['image']?>">
         <div class="quest" >
-            <legend>Question 7 : A quel personnage de la serie La Casa de Papel, cette ville à prêtée son nom? (la température actuelle de ce lieu est de
-                <?php echo $temp7;?>°C)</legend>
+            <legend>Question 7 <br><br> A quel personnage de la serie La Casa de Papel, cette ville à prêtée son nom?</legend>
             <button id="bad7">Rio</button>
             <button id="good7">Nairobi</button>
         </div>
     </div>
     <div id="question8" class="caché">
         <img src="<?php echo $photo8['image']?>">
+        <p>(la température actuelle de ce lieu est de <?php echo $temp8;?>°C)</p>
         <div class="quest" >
-            <legend>Question 8 : S'agit il d'une station météorologique ou d'un aéroport ? (la température actuelle de ce lieu est de
-                <?php echo $temp8;?>°C)</legend>
+            <legend>Question 8 <br><br> S'agit il d'une station météorologique ou d'un aéroport ?</legend>
             <button id="good8">Aéroport</button>
             <button id="bad8">Station météorologique</button>
         </div>
     </div>
-    <p id="confinement" class="caché">Le président Emmanuel Macron annonce la fin du confinement</p>
+    <h1 id="confinement" class="caché">Le président Emmanuel Macron annonce la fin du confinement</h1>
